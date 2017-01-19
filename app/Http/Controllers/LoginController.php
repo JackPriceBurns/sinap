d8859ff4e896a17fa5d8b8fa26775d40c70d6fac
Jack Price-Burns <jackprice-burns@jacks-imac.home>
2017-01-19 11:42:58 +0000

    Latest Version

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -11,26 +11,26 @@ class LoginController extends Controller
     public function login(Request $request){
 
         $student = Auth::guard('student');
-        $teacher = Auth::guard('teacher');
-        $admin = Auth::guard('admin');
+        //$teacher = Auth::guard('teacher');
+        //$admin = Auth::guard('admin');
 
-        if($student->check() || $teacher->check() || $admin->check()) {
+        if($student->check()) {
             return Redirect::to('/admin/overview')->send();
         }
 
         $credentials = ['email'=>$request->input('email'), 'password'=>$request->input('password')];
 
-        if($student->attempt($credentials)) {
+        if(Auth::guard('student')->attempt($credentials)) {
             //die("1");
             return Redirect::to('/student/overview')->send();
         }
 
-        if($teacher->attempt($credentials)) {
+        if(Auth::guard('teacher')->attempt($credentials)) {
             //die("2");
             return Redirect::to('/teacher/overview')->send();
         }
 
-        if($admin->attempt($credentials)) {
+        if(Auth::guard('admin')->attempt($credentials)) {
             //die("3");
             return Redirect::to('/admin/overview')->send();
         }
--- app/Teacher.php
+++ app/Teacher.php
@@ -4,8 +4,9 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Teacher extends Model
+class Teacher extends Authenticatable
 {
     use Notifiable;
 
