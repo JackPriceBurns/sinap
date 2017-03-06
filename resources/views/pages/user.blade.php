2a571021135fb9426b4c70d1f4897c7921243845
Jack Price-Burns <jackprice-burns@ITs-iMac.local>
2017-03-06 15:26:31 +0000

    Added search to users page

--- resources/views/pages/user.blade.php
+++ resources/views/pages/user.blade.php
@@ -9,38 +9,38 @@
         <h1>Users</h1>
 
         <hr />
-        @php
-            $x = 1;
-        @endphp
-        <table>
-        @foreach ($users as $user)
-            @if(($x/3) == (int) ($x/3))
-                <tr>
-            @endif
-            <td>
-                <div class="media">
-                    <a class="pull-left" href="/user/{{ $user['user']->id }}">
-                        <img class="media-object dp img-circle" src="http://placehold.it/500x500" style="width: 100px;height:100px;">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">{{ $user['user']->name }} <small> {{ ($user['user']->year == 'n/a') ? '' : 'Yr '.$user['user']->year }}</small></h4>
-                        <h5>Last Seen: {{ \App\Classes\Auth::lastSeen($user['user']->id) }}</h5>
-                        <hr style="margin:8px auto">
+        <div class="col-md-offset-8">
+            <input type="text" class='form-control' id="searchTable" placeholder="Search...">
+            <br />
+        </div>
+        <table class="table table-bordered" id="searchableTable">
+            <tbody>
+                @foreach ($users as $x => $user)
+                    @if(($x/3.0) == (int) ($x/3))
+                        <tr>
+                    @endif
+                    <td>
+                        <div class="media">
+                            <a class="pull-left" href="/user/{{ $user['user']->id }}">
+                                <img class="media-object dp img-circle" src="http://placehold.it/500x500" style="width: 100px;height:100px;">
+                            </a>
+                            <div class="media-body">
+                                <h4 class="media-heading">{{ $user['user']->name }} <small> {{ ($user['user']->year == 'n/a') ? '' : 'Yr '.$user['user']->year }}</small></h4>
+                                <h5>Last Seen: {{ \App\Classes\Auth::lastSeen($user['user']->id) }}</h5>
+                                <hr style="margin:8px auto">
 
-                        <span class="label label-danger">{{ $user['role']->name }}</span>
-                        @foreach ($user['badges'] as $badge)
-                            <span class="label label-{{ $badge->colour }}">{{ $badge->name }}</span>
-                        @endforeach
-                    </div>
-                </div>
-            </td>
-            @if(($x/3) == (int) ($x/3))
-                </tr>
-            @endif
-            @php
-                $x++;
-            @endphp
-        @endforeach
+                                <span class="label label-danger">{{ $user['role']->name }}</span>
+                                @foreach ($user['badges'] as $badge)
+                                    <span class="label label-{{ $badge->colour }}">{{ $badge->name }}</span>
+                                @endforeach
+                            </div>
+                        </div>
+                    </td>
+                    @if(($x+2/3.0) == (int) ($x+2/3))
+                        </tr>
+                    @endif
+                @endforeach
+            </tbody>
         </table>
     </div>
 
